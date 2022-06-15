22bedacbaf3adad8a57ecd094c4540cc64d65f99
Iana Pylaeva <MPiY@yandex.ru>
2022-06-15 23:04:58 +0300

    P13

--- app.js
+++ app.js
@@ -8,11 +8,7 @@ const bodyParser = require('body-parser');
 
 const { PORT = 3000 } = process.env;
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
