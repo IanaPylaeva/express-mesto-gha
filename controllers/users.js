f80b08fac8b65654e9274b559b020deddea652d9
Iana Pylaeva <MPiY@yandex.ru>
2022-06-15 22:57:12 +0300

    P13

--- controllers/users.js
+++ controllers/users.js
@@ -4,7 +4,7 @@ const User = require('../models/user');
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
-    .then(user => res.status(200).send(user))
+    .then((user) => res.status(200).send(user))
     .catch((error) => {
       if (error.name === 'ValidationError') {
         res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя' });
