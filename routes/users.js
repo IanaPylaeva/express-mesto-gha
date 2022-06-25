89d0635b0f95d0989f7ba9b614420efa0593e29c
Iana Pylaeva <MPiY@yandex.ru>
2022-06-26 02:05:51 +0300

    14

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 
 const {
   getAllUsers,
@@ -10,10 +11,24 @@ const {
 } = require('../controllers/users');
 
 router.get('/users', getAllUsers); // возвращает всех пользователей
-router.get('/users/:userId', getUserId); // возвращает пользователя по _id
+router.get('/users/:userId', celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().alphanum().required(),
+  }),
+}), getUserId); // возвращает пользователя по _id
 router.get('/users/me', getUserInfo); // возвращает информацию о текущем пользователе
 router.post('/users', createUser); // создаёт пользователя
-router.patch('/users/me', updateUser); // обновляет профиль
-router.patch('/users/me/avatar', updateAvatar); // обновляет аватар
+router.patch('/users/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30).required(),
+    about: Joi.string().min(2).max(30).required(),
+  }),
+}), updateUser); // обновляет профиль
+router.patch('/users/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string()
+      .pattern(/http[s]?:\/\/(www.)?[-a-zA-Z0-9._~:/?@!&'()*+,;=#]/).required(),
+  }),
+}), updateAvatar); // обновляет аватар
 
 module.exports = router;
