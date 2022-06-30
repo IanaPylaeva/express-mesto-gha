e0920c350ada5c4655de9cbd010afbc69ff01a81
Iana Pylaeva <MPiY@yandex.ru>
2022-06-30 15:10:53 +0300

    14

--- controllers/users.js
+++ controllers/users.js
@@ -128,7 +128,7 @@ module.exports.getUserInfo = (req, res, next) => {
       if (!user) {
         throw new NotFoundError('Пользователь по указанному _id не найден');
       }
-      return res.status(200).send(user);
+      return res.status(200).send({ data: user });
     })
     .catch((error) => {
       if (error.name === 'CastError') {
